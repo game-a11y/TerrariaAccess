d66630d66bb16df7cfb80f17c2a4b94f3dfe119e
Chengyu HAN <cyhan.dev@outlook.com>
2025-07-12 19:34:41 +0800

    hook: InGame Main_DrawSettingButton

--- TerrariaAccess.cs
+++ TerrariaAccess.cs
@@ -37,7 +37,27 @@ namespace TerrariaAccess
         {
             Terraria.UI.On_UIElement.MouseOver += UIElement_MouseOverHook;
             Terraria.On_IngameOptions.MouseOver += IngameOptions_MouseOverHook;
+            Terraria.On_Main.DrawSettingButton += Main_DrawSettingButton;
             //Terraria.On_Main.DrawInterface_39_MouseOver += Main_DrawInterface_39_MouseOverHook;
+        }
+
+        /**
+         * 游戏中》背包页》设置按钮
+         */
+        private static void Main_DrawSettingButton(On_Main.orig_DrawSettingButton orig, ref bool mouseOver, ref float scale, int posX, int posY, string text, string textSizeMatcher, Action clickAction)
+        {
+            orig(ref mouseOver, ref scale, posX, posY, text, textSizeMatcher, clickAction);
+            // DrawSettingButton 会逐渐增加 scale，实现文字渐变动画。
+            // scale >= 0.96 时，动画终止，处于保持悬停状态中，不再输出。
+            var isScaleIncEnd = scale >= 0.96;
+            // 通过递增步长确定范围，仅在最后一次缩放时输出
+            var outputOnce = scale >= (0.96 - 0.02);
+            var isMouseOver = mouseOver && isScaleIncEnd && outputOnce;
+            if (isMouseOver)
+            {
+                var a11yText = text;
+                Logger.Debug($"(InGame) SettingButton: {a11yText}");
+            }
         }
 
         private static void UIElement_MouseOverHook(On_UIElement.orig_MouseOver orig, UIElement self, UIMouseEvent evt)
