7cbab3180b720bdae2440daf8bb7be234759a541
Chengyu HAN <cyhan.dev@outlook.com>
2025-07-20 21:46:50 +0800

    sr: Add A11yOut.Speak

---
+++ A11yOut.cs
@@ -0,0 +1,27 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariaAccess;
+
+public class A11yOut
+{
+    private static ILog Logger = LogManager.GetLogger("tAccess.Speak");
+
+    public static void Speak(string text, string debugContext = "")
+    {
+        // TODO: 输出到 SR
+        if (string.IsNullOrEmpty(debugContext))
+        {
+            Logger.Info(text);
+        }
+        else
+        {
+            Logger.Info($"{text}" +
+                $"\n\t{debugContext}");
+        }
+    }
+}
